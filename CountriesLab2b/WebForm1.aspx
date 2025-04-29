d62285bb9d9612163c24fa71988370a6cc9cf2bf
VasileiosTsamtsidis <vasileios.tsamtsidis1@gmail.com>
2025-04-29 19:52:58 +0300

    First Commit
    A back-end test project with C#.

---
+++ CountriesLab2b/WebForm1.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CountriesLab2b.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h1>ΠΛΗΡΟΦΟΡΙΕΣ ΧΩΡΑΣ</h1>
+            <p>
+                Χώρα:&nbsp;&nbsp;&nbsp;
+                <asp:DropDownList ID="lstCountry" runat="server" AutoPostBack="True">
+                </asp:DropDownList>
+            </p>
+            <p>
+                Iso Code:&nbsp;&nbsp;&nbsp; <asp:Label ID="lblIso" runat="server"></asp:Label>
+            </p>
+            <p>
+                Πρωτεύουσα:&nbsp;&nbsp;&nbsp;
+                <asp:Label ID="lblCapital" runat="server"></asp:Label>
+            </p>
+            <p>
+                <asp:Image ID="imgCountry" runat="server" />
+            </p>
+            <p>
+                Κωδικός Διεθνούς Κλήσης:&nbsp;&nbsp;&nbsp;
+                <asp:Label ID="lblPhone" runat="server"></asp:Label>
+            </p>
+            <p>
+                Νόμισμα:&nbsp;&nbsp;&nbsp;
+                <asp:Label ID="lblCurrency" runat="server"></asp:Label>
+            </p>
+            <p>
+                <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Κλήση Υπηρεσίας" />
+            </p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ CountriesLab2b/WebForm1.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CountriesLab2b
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        org.oorsprong.www.CountryInfoService ct = new org.oorsprong.www.CountryInfoService();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                lstCountry.Items.Add("-Select-");
+                org.oorsprong.www.tCountryCodeAndName[] can = ct.ListOfCountryNamesByName();
+
+                for (int i = 0; i < can.Length; i++)
+                {
+                    lstCountry.Items.Add(can[i].sName);
+                }
+            }
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string strIsoCountry = ct.CountryISOCode(lstCountry.Text);
+            lblIso.Text = strIsoCountry;
+
+            string strCapital = ct.CapitalCity(strIsoCountry);
+            lblCapital.Text = strCapital;
+
+            string strCountryFlag = ct.CountryFlag(strIsoCountry);
+            imgCountry.ImageUrl = strCountryFlag;
+
+            string strPhone = ct.CountryIntPhoneCode(strIsoCountry);
+            lblPhone.Text = strPhone;
+
+            org.oorsprong.www.tCurrency curr =  ct.CountryCurrency(strIsoCountry);
+            lblCurrency.Text = curr.sName;
+        }
+    }
+}
